bc2fd086bb61215daceba76efe4c01e0fa4f4f2c
v-asoliev <Abdukodir_Soliev@epam.com>
2022-05-15 15:29:28 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JSBasics
+ https://learn.epam.com/myLearning/program?groupGuid=b4184d1c-6350-42d6-bacc-43814944979a
---
+++ src/index.js
@@ -0,0 +1,33 @@
+function task1() {
+    let text = 'I can eat bananas all day';
+    let start = text.indexOf('bananas');
+    let len = 'bananas'.length;
+    let slice = text.slice(start, start + len);
+    let upper = slice.toUpperCase();
+    alert(upper);
+
+    const cars = ["Saab", "Volvo", "BMW"];
+}
+function task2() {
+    const cars = ["Saab", "Volvo", "BMW"];
+
+    //-get "BMW" value 
+    const idx = cars.indexOf("BMW");
+    console.group(cars[idx]);
+
+    //-change the first item of cars 
+    cars[0] = "Subaru";
+    console.log(cars);
+
+    //-remove last item in the array 
+    cars.pop();
+    console.log(cars);
+
+    //-add "Audi" to the array 
+    cars.push("Audi");
+    console.log(cars);
+
+    //-splice "Volvo" and "BMW" 
+    cars.splice(0, 0, "Volvo", "BMW");
+    console.log(cars);
+}
